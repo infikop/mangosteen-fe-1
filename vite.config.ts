ec4f103c5f624eea5b7d250b7f7c18a7c1441cdf
infi <wefantasy@qq.com>
2023-10-30 21:28:59 +0800

    ignore dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/potato-fe-1/dist/',
+  // base:'/potato-fe-1/dist/', github部署时需要放入base 
   plugins: [vue()]
 })
