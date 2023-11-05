25c3b6df4505926c5277fd62418edd65b5fd80c4
infi <wefantasy@qq.com>
2023-11-05 23:06:49 +0800

    TSX配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/potato-fe-1/dist/', github部署时需要放入base 
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true
+    })
+  ]
 })
